ffb5ccbd6c71d5f3cc4590729fb3c1db3bdbb699
Driely Santos <santosdriely98@gmail.com>
2024-06-04 15:44:58 -0300

    Criando menu com dois links do react-router-dom

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import Inicio from "./paginas/Inicio";
-import SobreMim from "./paginas/SobreMim";
-
-const pagina = window.location.pathname === '/' ? <Inicio/> : <SobreMim />
-function App() {
-  return pagina;
-}
-
-export default App;
