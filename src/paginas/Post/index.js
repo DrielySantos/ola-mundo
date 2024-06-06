d0f8f2d1477ff07241011b2ae4a2a8d68859d36b
Driely Santos <santosdriely98@gmail.com>
2024-06-06 15:16:02 -0300

    desenvolvido pagina 404 e tratando Post não encontrado

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -3,20 +3,29 @@ import { useParams } from "react-router-dom";
 import posts from "json/posts.json"
 import PostModelo from "componentes/PostModelo";
 import ReactMarkdown from "react-markdown";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 export default function Post(){
     const parametros = useParams()
     const post = posts.find((post) => {
         return post.id === Number(parametros.id)
     })
+
+    if(!post){
+        return <NaoEncontrada/>
+    }
+
     return(
-        <PostModelo fotoCapa={`/assets/posts/${post.id}/capa.png`} titulo={post.titulo}>
-            <div className="post-markdown-container">
-                <ReactMarkdown>
-                    {post.texto}
-                </ReactMarkdown>
-            </div>
-        </PostModelo>
+        <PaginaPadrao>
+            <PostModelo fotoCapa={`/assets/posts/${post.id}/capa.png`} titulo={post.titulo}>
+                <div className="post-markdown-container">
+                    <ReactMarkdown>
+                        {post.texto}
+                    </ReactMarkdown>
+                </div>
+            </PostModelo>
+        </PaginaPadrao>
 
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Post from "paginas/Post";
+import NaoEncontrada from "./paginas/NaoEncontrada";
+
 
 function AppRoutes() {
   return (
@@ -15,11 +17,11 @@ function AppRoutes() {
 
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />}/>
-          <Route path="sobremim" element={<SobreMim />}/>
-          <Route path="posts/:id" element={<Post/>}/>
+          <Route path="sobremim" element={<SobreMim />}/>  
         </Route>
 
-        <Route path="*" element={<div>Página não encontrada</div>}/>
+        <Route path="posts/:id/*" element={<Post/>}/>
+        <Route path="*" element={<NaoEncontrada />}/>
       </Routes>
 
       <Rodape/>
